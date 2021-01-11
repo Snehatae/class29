a6e34d084cbea45aebb3704b9f5497854e70e76e
snehatae <thakkarsneha18@gmail.com>
2021-01-11 15:52:16 +0530

    made the catapult

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,10 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+
+        this.image1=loadImage("./sprites/sling1.png");
+        this.image2=loadImage("./sprites/sling2.png");
+        this.image3=loadImage("./sprites/sling3.png");
     }
 
     fly(){
@@ -16,11 +20,19 @@ class SlingShot{
     }
 
     display(){
+
+        image(this.image1,200,120,30,100);
+        image(this.image2,180,120,30,60);
+
+        
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(49,22,8);
+            line(pointA.x-15, pointA.y, pointB.x-15, pointB.y+35);
+            line(pointA.x-15, pointA.y, pointB.x+25, pointB.y+35);
+            image(this.image3,pointA.x-20, pointA.y-5);
         }
     }
     
